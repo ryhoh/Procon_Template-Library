b0ef06c8f3113e75350fc0afb9f4b2baeedc64a2
ryhoh <axis38akasira@gmail.com>
2019-02-09 16:36:44 +0900

    ファイル名変更，テンプレート変更
    c++のテンプレ・ライブラリのファイル名を変更
    c++テンプレのrrepの終了条件をi==0からi==-1に変更

---
+++ template.cpp
@@ -0,0 +1,33 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define rep(i, n) REP(i, 0, n)
+#define REP(i, k, n) for(auto i = k; i != n; i++)
+#define rrep(i, n) RREP(i, n, -1)
+#define RREP(i, n, k) for(auto i = n; i != k; i--)
+
+#define debug(x) cerr << #x << " " << x << endl
+#define print(x) cout << x;
+#define println(x) cout << x << endl;
+#define spc << " " <<
+
+#define mp make_pair
+#define mt make_tuple
+#define pf push_front
+#define pb push_back
+#define ppf pop_front
+#define ppb pop_back
+#define eb emplace_back
+#define np next_permutation
+
+using ll = long long;
+using Pii = pair<int, int>;
+using Tiii = tuple<int, int, int>;
+template<class T>using V = vector<T>;
+template<class T>using VV = vector<vector<T>>;
+using Vi = vector<int>;
+using VVi = vector<vector<int>>;
+
+int main() {
+   return 0;
+}
